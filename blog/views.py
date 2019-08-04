2c2edefa0a9e47faac8594c004d8c8ce094d31d3
Crisescode <zhaopanp2018@outlook.com>
2019-08-04 13:10:41 +0800

    update split page function

--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,15 @@ from .models import Blog, BlogType
 def blog_list(request):
     page_num = request.GET.get('page', 1) # 获取页码参数，get请求
     blogs_list = Blog.objects.all()
-    paginator = Paginator(blogs_list, 5)
+    paginator = Paginator(blogs_list, 5) # 每5篇进行分页
     page_content = paginator.get_page(page_num)
+    page_current = page_content.number # 获取当前页码
+    page_range = [i for i in range(page_current-2, page_content+3) if i > 0 and i < paginator.num_pages]
 
     context = {}
     context['blogs'] = page_content.object_list
     context['page_content'] = page_content
+    context['page_range'] = page_range
     context['blog_count'] = Blog.objects.all().count()
     context['blog_types'] = BlogType.objects.all()
     return render(request, "blog_list.html", context=context)
