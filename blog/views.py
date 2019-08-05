304fc4a91c523c7ee98f4ca5b8064e9f2b399d51
Crisescode <zhaopanp2018@outlook.com>
2019-08-05 21:28:34 +0800

    add classifiction number

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Count
 from django.conf import settings
 from .models import Blog, BlogType
 
@@ -23,13 +24,31 @@ def common_params(request, blogs_list):
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
+    # 获取博客分类的对应博客数量
+    '''
+    blog_types = BlogType.objects.all()
+    blog_type_list = []
+    for blog_type in blog_types:
+        blog_type.blog_count = Blog.objects.filter(blog_type=blog_type).count()
+        print("blog_type", blog_type.blog_count)
+        blog_type_list.append(blog_type)
+    '''
+    # 获取日期分类的对应博客数量
+    blog_dates = Blog.objects.dates('create_time', 'month', order='DESC')
+    blog_dates_dict = {}
+    for blog_date in blog_dates:
+        blog_count = Blog.objects.filter(create_time__year=blog_date.year,
+                                         create_time__month=blog_date.month).count()
+        blog_dates_dict[blog_date] = blog_count
+
     context = {}
     context['blogs'] = page_content.object_list
     context['page_content'] = page_content
     context['page_range'] = page_range
     context['blog_count'] = Blog.objects.all().count()
-    context['blog_types'] = BlogType.objects.all()
-    context['blog_dates'] = Blog.objects.dates('create_time', 'month', order='DESC')
+    # context['blog_types'] = blog_type_list
+    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
+    context['blog_dates'] = blog_dates_dict
 
     return context
 
