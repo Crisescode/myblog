6f834bfd699f6e6dad684847d132eb7c257186bb
zhaopp5 <zhaopp5@lenovo.com>
2019-08-17 14:07:57 +0800

    update count number

--- read_statistics/models.py
+++ read_statistics/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class ReadNum(models.Model):
     read_num = models.IntegerField(default=0)
-    blog = models.OneToOneField(Blog, on_delete=models.DO_NOTHING)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
