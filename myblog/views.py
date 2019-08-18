ba9e798b79100e6b3bbae7dd21ed90a62a174639
zhaopp5 <zhaopp5@lenovo.com>
2019-08-18 16:41:55 +0800

    update read number display

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from read_statistics.utils import get_seven_days_read_num
+from blog.models import Blog
 
 
 def home(request):
+    ct = ContentType.objects.get_for_model(Blog)
+    dates, read_nums = get_seven_days_read_num(ct)
+
     context = {}
+    context['dates'] = dates
+    context['read_nums'] = read_nums
     return render(request, 'home.html', context=context)
